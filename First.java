933ddb4d16efa1afb7bfe161426ce5cdb165d91d
PosinasettyPrrassena <159441581+PosinasettyPrrassena@users.noreply.github.com>
2024-03-06 13:25:45 +0530

    Update First.java

--- First.java
+++ First.java
@@ -1,6 +1,8 @@
 class First{
 	public static void main(string arg[])
 {
-	system.out.println("Hello Prasuuuu"){
+	system.out.println("Hello Prasuuuu");
+	system.out.println("Hello Prasuuuu");
+	{
 }
 }
